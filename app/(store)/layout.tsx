26313ed9f2c4cbc8d3572f030d8eb6d23a3f5574
Rakesh P <rakeshp@Rakeshs-MacBook-Air.local>
2024-11-08 01:43:17 +0530

    Chores: Product show

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/Header";
+import { SanityLive } from "@/sanity/lib/live";
 
 const geistSans = localFont({
   src: "../fonts/GeistVF.woff",
@@ -35,6 +36,7 @@ export default function RootLayout({
           <Header />
         {children}
         </main>
+        <SanityLive />
       </body>
     </html>
     </ClerkProvider>
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,4 +1,6 @@
+import { imageUrl } from "@/lib/imageUrl";
 import { Product } from "@/sanity.types";
+import Image from "next/image";
 import Link from "next/link";
 
 interface ProductCardProps {        
@@ -9,7 +11,38 @@ function ProductCard({ product }: ProductCardProps) {
     const isOutOfStock = product.stock !== undefined && product.stock !== null && product.stock <= 0;
   return (
     <Link href={`/products/${product.slug?.current}`} className={`group flex flex-col bg-white border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-200 overflow-hidden ${isOutOfStock ? 'opacity-50 pointer-events-none' : ''}`}>
-        Product
+
+        <div className="relative aspect-square w-full h-full overflow-hidden">
+            {product.images && (
+                <Image
+                    className="object-cover transition-transform duration-300 group-hover:scale-105"
+                    src={imageUrl(product.images[0]).url()}
+                    alt={product.name || 'Product Image'}
+                    fill
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                />
+            )}
+            {isOutOfStock && (
+                <div className="absolute inset-0 flex items-center justify-center bg-black opacity-50">
+                    <span className="text-white text-md font-semibold">Out of Stock</span>
+                </div>
+            )}
+        </div>
+
+        <div className="p-4">
+            <h2 className="text-lg font-semibold text-gray-800 truncate">
+                {product.name   }
+            </h2>
+            <p className="text-sm text-gray-600 mt-2 line-clamp-2">
+                {product.description?.map((block) => (
+                    block._type === 'block' ? 
+                    block.children?.map((child) => child.text).join('') : ''
+                ))}
+            </p>
+            <p className="mt-2 text-lg font-semibold text-gray-800">
+                ₹{product.price}
+            </p>
+        </div>
     </Link>
   )
 }
